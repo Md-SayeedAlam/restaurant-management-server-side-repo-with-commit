75188340e71b04b3a1c873bba639f9deec46e8d2
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2024-12-22 20:39:31 +0600

    added vercel.json , index.js file

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config();
+const app = express();
+const  port = process.env.PORT || 5000;
+
+// middleware
+
+app.use(cors())
+
+app.use(express.json())
+
+
+app.get('/',async(req,res)=>{
+    res.send('server is working')
+})
+
+
+app.listen(port,()=>{
+    console.log(`server is working on port:${port}`)
+})
