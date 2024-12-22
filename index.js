a9e557ce9eb7d7127c2333534a665d940663e4e8
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2024-12-22 21:59:57 +0600

    added mongodb server and get food items from database and use post method to store data in database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const  port = process.env.PORT || 5000;
 
 // middleware
@@ -9,6 +10,81 @@ const  port = process.env.PORT || 5000;
 app.use(cors())
 
 app.use(express.json())
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.amvbb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("restaurantDB");
+    const restaurantCollection = database.collection("restaurant"); 
+
+
+
+    app.get('/foods',async(req,res)=>{
+        const cursor = restaurantCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      })
+
+    app.post('/foods',async(req,res)=>{
+        const foodsItem = req.body;
+        const result = await restaurantCollection.insertOne(foodsItem)
+        res.send(result)
+    })
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.get('/',async(req,res)=>{
